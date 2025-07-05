77da6b6db106e6ec4d9b14d02ed55b0ee6c70518
thebookish <87561302+thebookish@users.noreply.github.com>
2025-07-06 01:41:21 +0600

    Update index.js

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ app.use('/api/family', familyRoutes);
 app.use(errorHandler);
 
 // Server
-const PORT = process.env.PORT || 5000;
-app.listen(5000, '0.0.0.0', () => {
+const PORT = process.env.PORT;
+app.listen(PORT, '0.0.0.0', () => {
   console.log("Server running on http://0.0.0.0:5000");
 });
 
